366692bf39a1ebf98e87bdc9e065c15d05407f1a
Nectarios <nectarios@nexxie.com>
2023-01-17 09:49:55 +0200

    changed from windows

--- src/app/meals.services.ts
+++ src/app/meals.services.ts
@@ -1,4 +1,104 @@
+
 export class MealServices {
-    meals = [
-    ]
+    // Meals
+meals = [
+    {
+        name: "Quakers boiled in Milk",
+        ingredients: ["oat","milk","marmelade/honey",],
+        apparatus: ["microwave", 'bowl', "spoon", "knife"],
+        description: "put quakers with milk inside a bowl, mix them up, and put them in the microwave until they bloat. Then add some honey on it"
+    },
+    {
+        name: "Fried Eggs with rice",
+        ingredients: ["eggs","olive oil","rice","maggie"],
+        apparatus: ["fry pan", "small plate", "spoon or fork", "knife"],
+        description: "fry some egg whites and mix them with boiled rice"
+    },
+    {
+        name: "Capers",
+        ingredients: ["bread","butter","marmelade/honey","turkey ham"],
+        apparatus: ["capers maker", "small plate", "spoon or fork", "knife"],
+        description: "classic capers"
+    },
+    {
+        name: "Cornflakes",
+        ingredients: ["milk","cornflakes"],
+        apparatus: ["bowl","spoon"],
+        description: "classic cornflakes"
+    },
+    {
+        name: "Weetabix",
+        ingredients: ["milk","weetabix"],
+        apparatus: ["bowl","spoon"],
+        description: "classic weetabix meal"
+    },
+    {
+        name: "Boiled corn with cold cuts",
+        ingredients: ["corn","turkey ham","butter","bacon"],
+        apparatus: ["pot","plate","knife","toothpicks"],
+        description: "boil some corn and eat it with butter and some cold cuts on the side"
+    },
+    {
+        name: "Sandwitch",
+        ingredients: ["bread","turkey ham","butter","lettuce","cheese","mayonnaise"],
+        apparatus: ["plate","knife"],
+        description: ""
+    },
+    {
+        name: "Sandwitch",
+        ingredients: ["bread","turkey ham","butter","lettuce","cheese","mayonnaise"],
+        apparatus: ["plate","knife"],
+        description: ""
+    }
+]
+
+// Sides
+sides = [
+    {
+        name: "Bananes",
+        ingredients: ["bananas"],
+        apparatus: ["small plastic bag"],
+    },    
+    {
+        name: "Protein Bars",
+        ingredients: ["protein bars"],
+        apparatus: [],
+    },
+    {
+        name: "Bread with peanut butter",
+        ingredients: ["bread","peanutbutter"],
+        apparatus: ["aluminum foil"],
+    },
+    {
+        name: "Turkey ham",
+        ingredients: ["turkey ham"],
+        apparatus: ["aluminum foil"],
+    },
+    {
+        name: "Almonds",
+        ingredients: ["almonds"],
+        apparatus: ["aluminum foil"],
+    },  
+    {
+        name: "Apples",
+        ingredients: ["apples"],
+        apparatus: ["small plastic bag"],
+    },
+    {
+        name: "Olives",
+        ingredients: ["olives"],
+        apparatus: [],
+    },
+    {
+        name: "Yoghurt",
+        ingredients: ["yoghurt"],
+        apparatus: ["spoon","plate or bowl"],
+    },
+    {
+        name: "Butter crackers",
+        ingredients: ["butter crackers"],
+        apparatus: [],
+    }
+
+]
 }
